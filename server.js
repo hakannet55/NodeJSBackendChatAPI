56deae622968296da03106e819ff8a171bc6ba5c
Hakan.Kuru <hakan.kuru@etiya.com>
2025-03-31 23:48:13 +0300

    getUsers-getMessages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // server.js
+authenticateToken = require('./jwt_token');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');  // CORS modülünü içe aktar
@@ -14,15 +15,29 @@ app.use(bodyParser.json());
 // CORS'u her türlü istek için aktif hale getirelim
 app.use(cors());
 
+
 // API rotaları
 app.post('/register', routes.register);
 app.post('/login', routes.login);
-app.post('/message', routes.sendMessage);
-// test playlogrund
-
+// authenticateToken ?? kontrolü de eklencek
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
+//app.get('/rooms', routes.getRooms,authenticateToken);
+
+app.get('/messages/:userId', routes.getMessages,authenticateToken);
+app.get('/users', routes.getUsers,authenticateToken);
+
+//app.get('/api/rooms/:roomId', routes.getRoomById);
+//app.get('/api/rooms/:roomId/users', routes.getRoomUsers);
+//app.get('/api/rooms/:roomId/messages', routes.getRoomMessages);
+
+
+app.get('prodductsApp/getAllProducts', routes.getAllProducts);
+//app.get('prodductsApp/addProduct', routes.getAllProducts);
+//app.get('prodductsApp/removeProduct', routes.getAllProducts);
+//app.get('prodductsApp/updateProduct', routes.getAllProducts);
 
 // Sunucuyu 3000 portunda dinlemeye başla
 app.listen(3000, () => {
